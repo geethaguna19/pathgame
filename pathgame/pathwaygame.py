4cef6922a08877aa09ad8ace3676a9fa93475b3f
geethaguna19 <157560448+geethaguna19@users.noreply.github.com>
2024-03-26 23:37:09 +0530

    Update pathwaygame.py

--- pathgame/pathwaygame.py
+++ pathgame/pathwaygame.py
@@ -1,20 +1,18 @@
 name=input("enter your name:")
 print("Hello",name,"welcome to the path waygame .")
 answer=input("This has to be end enter whether you want to go left/right or back:").lower()
-while answer!= "quit":
-
-    if answer=="left":
-        answer=input("you came to a river you have to decide now whether you have to cross or go back")
-        if answer=="cross":
+if answer=="left":
+    answer=input("you came to a river you have to decide now whether you have to cross or go back")
+    if answer=="cross":
             print("the river is so deep you fell into the river and lost")
             quit()
-        elif answer=="back":
+    elif answer=="back":
             print("you went back and lost the game")
             quit()
-        else:
+    else:
             print("invalid option you lost")
             quit()
-    elif answer=="right":
+elif answer=="right":
          answer=input("you came near a bridge now do yo want to walk on the bridge or cross the bridge ")
          if answer=="walk":
              print("you have walked many miles and you lost the game")
@@ -30,6 +28,6 @@ while answer!= "quit":
              else:
                  print("invalid answert you loss the game")
                  quit()
-    else:
+else:
         print("you go back and lost the game")
-    print("thank you for playing",name)
+print("thank you for playing",name)
